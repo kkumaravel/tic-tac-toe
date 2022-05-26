8074f9042f3274b258590c241174d51cb2fb87ec
kkumaravel <kavin@hausanalytics.com>
2022-05-26 13:53:47 -0700

    Change Squares to Square and clicks alternate between X and O

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-function Squares(props) {
+function Square (props) {
   return (
     <button className = 'square' onClick = {props.onClick}>{props.value}</button>
   );
@@ -12,13 +12,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i){
     const squares = this.state.squares.slice();
-    squares[i] = 'X'
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
 
